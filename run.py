b5842172cd8790ac5346aa343826f8df19c0f5c3
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2017-07-17 18:44:52 -0700

    Created a start to a usable user interface. I need to sperate these into seperate files and make it possible to output to files

--- run.py
+++ run.py
@@ -168,7 +168,7 @@ class Pool:
     #print each schedule out for the teams
     def printAllSchedules(self):
         for i in range(len(self.__teamList)):
-            print "Team ",self.__teamList[i].getID()," ",
+            print "Team ",self.__teamList[i].getID()," "
             self.__teamList[i].printSchedule()
 
     #prints out a single team schedule
@@ -176,6 +176,7 @@ class Pool:
     def printTeamSchedule(self,teamNo):
         print "Team ",self.__teamList[teamNo].getID()," ",
         self.__teamList[teamNo].printSchedule()
+        print
 
     #gets the teams from the schedule
     def getTeam(self,num):
@@ -387,23 +388,70 @@ class FormatBracket(Format):
 #the user interface of the software
 class User:
     def __init__(self):
-        pass
+        self.tmpBracket = Bracket()
+        self.formattedBracket = FormatBracket(self.tmpBracket)
 
+    #the user interface for the user
     def beginning(self):
-        into = input("Options:\n"
-        "1. Make Bracket")
-    def useBracket(self):
-        gamesNum = input("Number of games per team to play.")
-        teamsNum = input("Number of teams in the tournament.")
-        poolsNum = input("Number of pools in the tourament.")
+        into = 0
+        while(into != 6):
+            into = input("Options:\n"
+            "1. Make New Bracket:\n"
+            "2. Print Team Schedules:\n"
+            "3. Print Tourament Schedule:\n"
+            "4. Change a Team Name: \n"
+            "5. End the Session\n")
 
+            if(into == 1):
+                self.useBracket()
+            elif(into == 2):
+                self.printSchedules(into)
+            elif(into == 3):
+                self.printSchedules(into)
+            elif(into == 4):
+                self.changeTeamName()
+
+
+    #gets the data structures ready for output
+    def output(self,fileType):
+        if(fileType == "excel"):
+            #add tabs to it
+            pass
+
+
+    #making a formatting bracket
+    def useBracket(self):
+        try:
+            gamesNum = input("Number of games per team to play: ")
+            teamsNum = input("Number of teams in the tournament: ")
+            poolsNum = input("Number of pools in the tourament: ")
+            self.tmpBracket.makeBracket(gamesNum,teamsNum,poolsNum)
+            tmpFB = FormatBracket(self.tmpBracket)
+            self.formattedBracket = tmpFB
+        except:
+            print "Please give a valid information."
+
+    #changes the name of a team
+    #teamNum: the team number to replace
+    #teamName: The name that the team will be replaced to.
+    def changeTeamName(self):
+        teamNum = input("Enter the Team Number to change: ")
+        teamName = str(raw_input("Please enter the Team Name: "))
+
+        self.formattedBracket.__changeTeamName__(teamNum,teamName)
+
+    #printType: 2 for Full Tournament Schedule
+    #printType: 3 for Pools Schedule per Team
+    #prints all of the Schedules
+    def printSchedules(self,printType):
+        if(printType == 2):
+            print "Full Tournament Schedule: "
+            self.formattedBracket.printSchedule("A")
+            print
+        elif(printType == 3):
+            print "Pool Play for each Team:"
+            self.formattedBracket.printSchedule("s")
 
 if(__name__ == "__main__"):
-
-    B = Bracket()
-    F = Pool()
-    B.makeBracket(5,16,2)
-    FB = FormatBracket(B)
-    FB.__changeTeamName__(1,"drew")
-    FB.__changeTeamName__(16,"Max")
-    FB.printSchedule("s")
+    U = User()
+    U.beginning()
